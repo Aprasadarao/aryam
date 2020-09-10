fdea447121bfbe10520113eb6f5ee7277e926baa
Aprasadarao <prasadarao04@gmail.com>
2020-09-10 16:10:48 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,55 @@
+/*
+
+var nav = document.getElementById('nav');
+
+window.onscroll = function () {
+
+  if(window.pageYOffset > 33){
+
+   nav.style.position = "fixed";
+   nav.style.top = 0;
+
+   }else{
+     nav.style.position = "absolute";
+     nav.style.top = 0;
+
+   }
+} */
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+  }
+}
+
